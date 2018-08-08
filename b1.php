c23324ac6652aa2dac98ee1bbe0122d3ad104803
harsha <vardhanharsha103@gmail.com>
2018-08-08 22:26:49 +1000

    modified b.php file

--- b1.php
+++ b1.php
@@ -3,3 +3,23 @@
 <?php echo $_POST['entered']; ?>
 </body>
 </html>
+<html>
+<body>
+<?php echo $_POST['entered']; ?>
+</body>
+</html>
+<html>
+<body>
+<?php echo $_POST['entered']; ?>
+</body>
+</html>
+<html>
+<body>
+<?php echo $_POST['entered']; ?>
+</body>
+</html>
+<html>
+<body>
+<?php echo $_POST['entered']; ?>
+</body>
+</html>
